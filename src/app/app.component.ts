5ae6df3f31ba29e93694df05301ccb652fcd4806
kadarH <kadar.hamza@gmail.com>
2019-10-16 18:54:25 +0100

    Add side navbar & Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'truckMe-app';
+
+  events: string[] = [];
+  opened: boolean;
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatNativeDateModule} from '@angular/material/core';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [SidenavComponent],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule { }
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
